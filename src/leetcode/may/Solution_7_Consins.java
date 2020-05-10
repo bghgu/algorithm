7454b1b73ad3d3961b0c12211bdb3baee26b2ec0
bghgu <ektmf1993@gmail.com>
2020-05-10 23:44:40 +0900

    0510

---
+++ src/leetcode/may/Solution_7_Consins.java
@@ -0,0 +1,9 @@
+package leetcode.may;
+
+/**
+ * Created by ccoli on 2020/05/09.
+ */
+
+public class Solution_7_Consins {
+
+}
---
+++ src/leetcode/may/Solution_8_CheckIfItIsAStraightLine.java
@@ -0,0 +1,16 @@
+package leetcode.may;
+
+/**
+ * Created by ccoli on 2020/05/09.
+ */
+
+public class Solution_8_CheckIfItIsAStraightLine {
+
+    public static void main(String... args) {
+
+    }
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        return true;
+    }
+}
