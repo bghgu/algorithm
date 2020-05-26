1ef68e0006f32c32a72e1b14fe6cde1cafab8fc8
bghgu <ektmf1993@gmail.com>
2020-05-26 19:54:41 +0900

    0526

---
+++ src/leetcode/may/Solution_26_.java
@@ -0,0 +1,17 @@
+package leetcode.may;
+
+/**
+ * Created by ccoli on 2020/05/26.
+ */
+
+public class Solution_26_ {
+
+    public static void main(String... args) {
+        char a = 'a';
+        System.out.println(a); //a
+        int b = (char) a;
+        System.out.println(b); //97
+        char c = a;
+        System.out.println(c); //a
+    }
+}
