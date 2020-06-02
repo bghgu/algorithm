30d8c5a6d1f1acf86173d6d75e8f744deb3f3bbc
bghgu <ektmf1993@gmail.com>
2020-06-02 21:55:59 +0900

    0602

---
+++ src/leetcode/june/Solution_2_DeleteNodeInALinkedList.java
@@ -0,0 +1,30 @@
+package leetcode.june;
+
+/**
+ * Created by ccoli on 2020/06/02.
+ */
+
+public class Solution_2_DeleteNodeInALinkedList {
+
+    public static void main(String... args) {
+        ListNode listNode = new ListNode(4);
+        listNode.next = new ListNode(5);
+        listNode.next.next = new ListNode(1);
+        listNode.next.next.next = new ListNode(9);
+        deleteNode(listNode.next);
+    }
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
