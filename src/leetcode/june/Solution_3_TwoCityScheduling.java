4936c31ff8acf037dedc9dd092bd19315674f7cf
bghgu <ektmf1993@gmail.com>
2020-06-03 18:36:28 +0900

    0603

---
+++ src/leetcode/june/Solution_3_TwoCityScheduling.java
@@ -0,0 +1,16 @@
+package leetcode.june;
+
+/**
+ * Created by ccoli on 2020/06/03.
+ */
+
+public class Solution_3_TwoCityScheduling {
+
+    public static void main(String... args) {
+
+    }
+
+    public static int twoCitySchedCost(int[][] costs) {
+
+    }
+}
