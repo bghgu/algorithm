c55fb7bc742186eb1ebaefb0b745411ca05cd2d7
bghgu <ektmf1993@gmail.com>
2020-06-18 19:07:42 +0900

    0618

---
+++ src/leetcode/june/Solution_18_H_Index2.java
@@ -0,0 +1,16 @@
+package leetcode.june;
+
+/**
+ * Created by ccoli on 2020/06/18.
+ */
+
+public class Solution_18_H_Index2 {
+
+    public static void main(String... args) {
+
+    }
+
+    public static int hIndex(int[] citations) {
+        return 0;
+    }
+}
