0a8b7e49cbc43b01bab46e2544c93e131ee7c127
bghgu <ektmf1993@gmail.com>
2020-06-26 17:16:33 +0900

    0626 fail

---
+++ src/leetcode/june/Solution_26_SumRootToLeafNumbers.java
@@ -0,0 +1,35 @@
+package leetcode.june;
+
+/**
+ * Created by ccoli on 2020/06/26.
+ */
+
+public class Solution_26_SumRootToLeafNumbers {
+
+    public static void main(String... args) {
+        
+    }
+
+    public int sumNumbers(TreeNode root) {
+        return 0;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
