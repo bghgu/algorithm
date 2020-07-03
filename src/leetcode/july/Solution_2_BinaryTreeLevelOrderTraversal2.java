c676accc729d722b3d626bdfc7358f4b4d7b5062
bghgu <ektmf1993@gmail.com>
2020-07-03 09:41:48 +0900

    0703

--- src/leetcode/july/Solution_2_BinaryTreeLevelOrderTraversal2.java
+++ src/leetcode/july/Solution_2_BinaryTreeLevelOrderTraversal2.java
@@ -1,5 +1,6 @@
 package leetcode.july;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,17 +11,42 @@ import java.util.List;
 public class Solution_2_BinaryTreeLevelOrderTraversal2 {
 
     public static void main(String... args) {
-
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(9);
+        treeNode.right = new TreeNode(20);
+        treeNode.right.left = new TreeNode(15);
+        treeNode.right.right = new TreeNode(7);
+        System.out.println(levelOrderBottom(treeNode));
     }
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> result = new LinkedList<>();
-        TreeNode now = root;
+    //즉, 왼쪽에서 오른쪽으로, 리프에서 루트까지 레벨별로 레벨을 조정하십시오
 
+    private static List<List<Integer>> result;
+    private static int maxLevel;
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        result = new LinkedList<>();
+        if (root == null) return result;
+        maxLevel = 0;
+        func(root, 0);
+        for (int i = 0; i <= maxLevel; i++) {
+            result.add(new LinkedList<>());
+        }
+        func(root, 0);
+        Collections.reverse(result);
         return result;
     }
 
-    public class TreeNode {
+    private static void func(final TreeNode now, final int level) {
+        if (now.left != null) func(now.left, level + 1);
+        if (now.right != null) func(now.right, level + 1);
+        maxLevel = Math.max(level, maxLevel);
+        if (result.size() != 0) {
+            result.get(level).add(now.val);
+        }
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
