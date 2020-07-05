3bd8c44c6d16266c6c5f23d1ef6ac75a5dae30eb
bghgu <ektmf1993@gmail.com>
2020-07-05 23:30:13 +0900

    0705

---
+++ src/leetcode/july/Solution_5_HammingDistance.java
@@ -0,0 +1,16 @@
+package leetcode.july;
+
+/**
+ * Created by ccoli on 2020/07/05.
+ */
+
+public class Solution_5_HammingDistance {
+
+    public static void main(String... args) {
+
+    }
+
+    public static int hammingDistance(int x, int y) {
+        return 0;
+    }
+}
