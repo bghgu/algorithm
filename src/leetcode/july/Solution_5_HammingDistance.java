be9059172902e9e5cde20b884f8c7df271ba0c3e
bghgu <ektmf1993@gmail.com>
2020-07-06 15:44:18 +0900

    0706

--- src/leetcode/july/Solution_5_HammingDistance.java
+++ src/leetcode/july/Solution_5_HammingDistance.java
@@ -7,10 +7,29 @@ package leetcode.july;
 public class Solution_5_HammingDistance {
 
     public static void main(String... args) {
-
+        System.out.println(hammingDistance(1, 4));
     }
 
     public static int hammingDistance(int x, int y) {
-        return 0;
+        String a = Integer.toBinaryString(x);
+        String b = Integer.toBinaryString(y);
+        if (a.length() < b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        String temp = "";
+        for (int i = 0; i < a.length() - b.length(); i++) {
+            temp += "0";
+        }
+        temp += b;
+        b = temp;
+        int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
